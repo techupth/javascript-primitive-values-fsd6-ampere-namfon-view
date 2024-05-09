4965c782631e248d1957e6c022b37fafcce8ba64
Namfon <varisa.vb@hotmail.com>
2024-05-09 14:07:26 +0700

    feat(ex5): declare price and discount

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discountValue = 0.25 * totalPrice;
+console.log(discountValue);
+let discountPrice = totalPrice - discountValue;
+console.log(discountPrice);
