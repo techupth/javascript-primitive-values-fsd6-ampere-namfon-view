14542b7a34fc2fb01ebd926580b62095956dbfad
revieww12 <review120244@gmail.com>
2024-05-09 13:52:32 +0700

    feat: JavaScript Primitive Values Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = (25 / 100) * totalPrice;
+let finalPrice = totalPrice - discount;
+console.log(finalPrice);
